6827064c69bab80f86a94a0f73352f42be70ba3b
henzogabriel954 <gabrielhenzo316@gmail.com>
2026-02-26 19:35:44 -0300

    Meu primeiro commit

---
+++ src/Variavel.java
@@ -0,0 +1,10 @@
+import java.time.*;
+import java.util.*;
+public class Variavel {
+
+    public static void main(String[] args) {
+
+        int horaAtual = LocalDateTime.now().getHour();
+        System.out.println(horaAtual);
+    }
+}
